b93c9fa0d4b2df9ea007c019db9e40439a8332f8
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2017-02-02 21:40:50 -0300

    trying to get start with lab03

---
+++ IdeaProjects/SpringBoot/src/main/java/br/com/project/tasklist/controller/TaskController.java
@@ -0,0 +1,11 @@
+package br.com.project.tasklist.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by gersonsales on 02/02/17.
+ */
+@Controller
+public class TaskController {
+
+}
